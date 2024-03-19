669641cfce3cc8d6bd624cfc2089ee43f05b1532
Anup-1827 <anupalone.off@gmail.com>
2024-03-19 08:25:32 +0530

    Navigation Buttons are done

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import bg from "../public/background/home-background.png"
 import RenderModel from "@/component/RenderModel";
 import Wizard from "@/component/models/Wizard";
+import Navigation from "@/component/navigation";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between relative">
@@ -14,7 +15,8 @@ export default function Home() {
         className="-z-50 w-full h-full object-cover object-center"
       />
 
-      <div className="h-screen">
+      <div className="w-full h-screen">
+          <Navigation/>
           <RenderModel>
             <Wizard/>
           </RenderModel>
---
+++ component/navigation/index.jsx
@@ -0,0 +1,35 @@
+"use client"
+
+import { BtnList } from '@/lib/data'
+import React from 'react'
+import NavButton from './NavButton';
+
+function Navigation() {
+    // const angleIncrement = 360 / BtnList.length;
+
+    const radianIncrement = 2 * Math.PI / BtnList.length;
+    const radius =  window.innerWidth * 0.2 - 16;  
+
+  return (
+    <div className='w-full fixed h-screen flex items-center justify-center'>
+        <div className='w-max flex items-center justify-center relative group text-white animate-spin-slow hover:pause'>
+            {
+                BtnList.map((btn, index)=>{
+                    const angle = index * radianIncrement;
+                    const x = Math.cos(angle) * radius
+                    const y = Math.sin(angle) * radius
+                    // const angleRad = (index * angleIncrement * Math.PI) / 180;
+                    // const radius = "calc(20vw - 1rem)";
+
+                    // const x = `calc(${radius}*${Math.cos(angleRad)})`;
+                    // const y = `calc(${radius}*${Math.sin(angleRad)})`;
+                    
+                    return( <NavButton key={index} x={x} y={y} {...btn}/>)
+                })
+            }
+        </div>
+    </div>
+  )
+}
+
+export default Navigation

5809ba2197450cde43a40759df3d25101240bfe4
Anup-1827 <anupalone.off@gmail.com>
2024-03-21 23:07:17 +0530

    Layout has been Created

--- app/(sub pages)/layout.js
+++ app/(sub pages)/layout.js
@@ -1,23 +1,9 @@
 import React from 'react'
 
-import HomeButton from "@/component/HomeButton";
-import Image from "next/image";
-import bg from "../../public/background/projects-background.png"
-
-// "../../../public/background/projects-background.png";
-
 
 function SubpageLayout({children}) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between relative">
-    <Image
-      priority
-      sizes="100vw"
-      src={bg}
-      alt="homebackground"
-      className="-z-50 w-full h-full fixed object-cover object-center grayscale-[30%]"
-    />
-    <HomeButton/>
     {children}
    </main>
   )
--- app/(sub pages)/projects/page.js
+++ app/(sub pages)/projects/page.js
@@ -3,10 +3,22 @@ import FireFliesBackground from "@/component/FireFliesBackground";
 import Staff from "@/component/models/Staff";
 import Projects from "@/component/projects";
 import { projectsData } from "@/lib/data";
+
+import HomeButton from "@/component/HomeButton";
+import Image from "next/image";
+import bg from "../../../public/background/projects-background.png"
+
 export default function Page() {
   return (
     <>
-    
+    <Image
+      priority
+      sizes="100vw"
+      src={bg}
+      alt="homebackground"
+      className="-z-50 w-full h-full fixed object-cover object-center grayscale-[30%]"
+    />
+    <HomeButton/>
     <Projects projectsData={projectsData}/>
 
     <div className="flex items-center justify-center fixed  top-16  lg:top-20 -translate-x-1/2 lg:translate-x-0 -z-10 left-1/2 lg:-left-16 h-screen">

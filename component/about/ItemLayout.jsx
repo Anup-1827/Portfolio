4271405d64c4e366180176b4bce68effa775fca1
Anup-1827 <anupalone.off@gmail.com>
2024-03-22 09:13:49 +0530

    About page is done

--- component/about/ItemLayout.jsx
+++ component/about/ItemLayout.jsx
@@ -5,8 +5,11 @@ import clsx from "clsx";
 const ItemLayout = ({ children, className }) => {
   return (
     <div
-      className="custom-bg p-6 sm:p-8 rounded-xl flex items-center justify-center space-y-8">
-      {children}
+    className={clsx(
+      "custom-bg p-6 sm:p-8 rounded-xl flex items-center justify-center space-y-8",
+      className
+    )}>
+    {children}
     </div>
   );
 };
--- component/about/index.jsx
+++ component/about/index.jsx
@@ -1,12 +1,46 @@
 import React from 'react'
+import ItemLayout from './ItemLayout'
 
 function AboutDetails() {
   return (
     <div className='absolute w-full max-w-7xl top-full text-white grid  grid-cols-12 gap-5'>
-        <div className='col-span-8 row-span-4 custom-bg flex justify-center items-center rounded-xl'>Info</div>
-        <div className='col-span-4 row-span-2 custom-bg flex justify-center items-center rounded-xl'>Info</div>
-        <div className='col-span-4 row-span-2 custom-bg flex justify-center items-center rounded-xl'>Info</div>
-        {/* <div></div> */}
+        <ItemLayout
+          className={
+            " col-span-full lg:col-span-8 row-span-2 flex-col items-start"
+          }
+        >
+          <h2 className="  text-xl md:text-2xl text-left w-full capitalize">
+            Architect of Enchantment
+          </h2>
+          <p className="font-light  text-xs sm:text-sm md:text-base   ">
+            My journey in web development is powered by an array of mystical
+            tools and languages, with JavaScript casting the core of my
+            enchantments. I wield frameworks like React.js and Next.js with
+            precision, crafting seamless portals (websites) that connect realms
+            (users) across the digital universe. The ancient arts of the
+            Jamstack empower me to create fast, secure, and dynamic experiences,
+            while my design skills ensure every creation is not only functional
+            but visually captivating. Join me as I continue to explore new
+            spells and technologies to shape the future of the web.
+          </p>
+        </ItemLayout>
+        <ItemLayout
+          className={" col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+        >
+          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
+            25+ <sub className="font-semibold text-base">clients</sub>
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+          className={"col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+        >
+          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
+            4+{" "}
+            <sub className="font-semibold text-base">years of experience</sub>
+          </p>
+        </ItemLayout>
+
     </div>
   )
 }

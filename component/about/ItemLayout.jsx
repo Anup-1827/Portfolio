fd520a4111d93fb60312cd1987b4419a1cd6f5f7
Anup-1827 <anupalone.off@gmail.com>
2024-03-23 16:51:25 +0530

    About Page Done

--- component/about/ItemLayout.jsx
+++ component/about/ItemLayout.jsx
@@ -4,13 +4,19 @@ import clsx from "clsx";
 
 const ItemLayout = ({ children, className }) => {
   return (
-    <div
-    className={clsx(
+    <motion.div
+      initial={{scale:0}}
+      whileInView={{scale:1}}
+      transition={{
+        duration: 0.5,
+      }}
+      viewport={{once:true}}
+      className={clsx(
       "custom-bg p-6 sm:p-8 rounded-xl flex items-center justify-center space-y-8",
       className
     )}>
     {children}
-    </div>
+    </motion.div>
   );
 };
 

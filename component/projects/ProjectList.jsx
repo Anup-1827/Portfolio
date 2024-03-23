25231c54d47fb25091779a8efb637a1635f49def
Anup-1827 <anupalone.off@gmail.com>
2024-03-23 17:06:02 +0530

    Projects are animatd

--- component/projects/ProjectList.jsx
+++ component/projects/ProjectList.jsx
@@ -1,8 +1,26 @@
+import { motion } from "framer-motion";
+import Link from "next/link";
 import React from "react";
+
+
+const item = {
+  hidden: {
+    opacity:0, y: 100
+  },
+  show:{
+    opacity:1, y:0
+  }
+}
+
+const ProjectLink = motion(Link)
 
 function ProjectLayout({ name, description, date, demoLink }) {
   return (
-    <div className=" text-sm md:text-base flex  items-center justify-between w-full relative rounded-lg overflow-hidden p-4 md:p-6 custom-bg">
+    <ProjectLink 
+      variants={item}
+      href={demoLink}
+      target={"_blank"}
+      className=" text-sm md:text-base flex  items-center justify-between w-full relative rounded-lg overflow-hidden p-4 md:p-6 custom-bg">
       <div className="flex items-center justify-center space-x-2">
         <h2 className="text-foreground">{name}</h2>
         <p className="text-muted hidden sm:inline-block">{description}</p>
@@ -11,7 +29,7 @@ function ProjectLayout({ name, description, date, demoLink }) {
       <p className="text-muted sm:text-foreground">
         {new Date(date).toDateString()}
       </p>
-    </div>
+    </ProjectLink>
   );
 }
 
--- component/projects/index.jsx
+++ component/projects/index.jsx
@@ -1,13 +1,32 @@
+"use client"
+import { motion } from 'framer-motion'
 import ProjectLayout from './ProjectList'
 
 
+const container = {
+  hidden: {
+    opacity :0
+  },
+  show:{
+    opacity: 1,
+    transition:{
+      staggerChildren: 0.3,
+      delayChildren: 1
+    }
+  }
+}
+
 function Projects({projectsData}) {
   return (
-    <div className='w-full max-w-auto max-w-2xl xl:max-w-4xl px-4 py-10 pt-20 mx-auto lg:px-16 space-y-6 md:space-y-8 flex flex-col items-center'>
+    <motion.div 
+      variants={container}
+      initial="hidden"
+      animate="show"
+      className='w-full max-w-auto max-w-2xl xl:max-w-4xl px-4 py-10 pt-20 mx-auto lg:px-16 space-y-6 md:space-y-8 flex flex-col items-center'>
         {
             projectsData.map((project, index)=> <ProjectLayout key={index} {...project}/>)
         }
-    </div>
+    </motion.div>
   )
 }
 
